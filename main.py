e0762614a3641f4f2b2517eec9936f4092cfc19e
AleksandrBelous <ab21082001@mail.ru>
2023-11-25 19:12:19 -0500

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,60 @@
-import Basic_System_Details
+import flet as ft
+import report
+
+
+def main(page: ft.Page):
+    page.title = 'Total Scan'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    possibilities = [
+        "Общие данные о системе",
+        "Системное время",
+        "Пользователи",
+        "Ядра ОС",
+        "Процессоры",
+        "HDD/SSD устройства",
+        "CD/DVD устройства",
+        "USB устройства",
+        "Данные о GPU",
+        "Данные о RAM памяти",
+        "Сетевые настройки",
+        "Приложения",
+        "Исполняемые файлы PATH"
+    ]
+
+    keys = set()
+
+    def all_selected(_):
+        for e in menu[1:-1]:
+            e.value = True
+            keys.add(e.label)
+        page.update()
+
+    def box_clicked(box: ft.ControlEvent):
+        if box.control.value:
+            keys.add(box.control.label)
+        else:
+            keys.remove(box.control.label)
+        print(keys)
+        page.update()
+
+    def start_button_clicked(_):
+        report.append_to_json(sorted(keys, key=lambda x: possibilities.index(x)))
+        page.update()
+
+    menu = [ft.Checkbox(label="Выбрать всё", label_position=ft.LabelPosition.LEFT, on_change=all_selected)]
+    menu += [ft.Checkbox(label=p, on_change=box_clicked) for p in possibilities[0:-2]]
+    menu += [ft.Checkbox(label="Приложения", on_change=box_clicked, label_position=ft.LabelPosition.LEFT)]
+    menu += [ft.Checkbox(label="Исполняемые файлы PATH", on_change=box_clicked, label_position=ft.LabelPosition.LEFT)]
+    menu.append(ft.ElevatedButton(text="Запуск", on_click=start_button_clicked))
+
+    page.add(
+        ft.Row([
+            ft.Column(menu)
+        ],
+            alignment=ft.MainAxisAlignment.CENTER)
+    )
+
 
 if __name__ == '__main__':
-    ...
+    ft.app(target=main)
--- report.py
+++ report.py
@@ -11,24 +11,25 @@ import System_Uptime
 import Users_Info
 
 
-def append_to_json() -> None:
+def append_to_json(keys: list) -> None:
     json_file_path = 'report.json'
+    audit_dct = {
+        "Общие данные о системе": Basic_System_Details.get_Common_Basic_System_Details(),
+        "Системное время": System_Uptime.get_Unix_System_Uptime_Info(),
+        "Пользователи": Users_Info.get_Common_Users_Info(),
+        "Ядра ОС": CPU_Info.get_Common_Cores_Info(),
+        "Процессоры": CPU_Info.get_Unix_CPU_Info(),
+        "HDD/SSD устройства": Disk_Info.get_Common_Disk_Info(),
+        "CD/DVD устройства": Disk_Info.get_CD_DVD_info(),
+        "USB устройства": Peripherals_Info.get_Unix_USB_Devices(),
+        "Данные о GPU": GPU_Info.get_Unix_CGPU_Info(),
+        "Данные о RAM памяти": Memory_Info.get_Common_Memory_Info(),
+        "Сетевые настройки": Network_Info.get_Common_Network_Info(),
+        "Приложения": Software_Info.get_xdg_open_applications(),
+        "Исполняемые файлы PATH": Software_Info.get_executables_in_PATH(),
+    }
     try:
-        report = {
-            "Общие данные о системе": Basic_System_Details.get_Common_Basic_System_Details(),
-            "Системное время": System_Uptime.get_Unix_System_Uptime_Info(),
-            "Пользователи": Users_Info.get_Common_Users_Info(),
-            "Ядра ОС": CPU_Info.get_Common_Cores_Info(),
-            "Процессоры": CPU_Info.get_Unix_CPU_Info(),
-            "HDD/SSD устройства": Disk_Info.get_Common_Disk_Info(),
-            "CD/DVD устройства": Disk_Info.get_CD_DVD_info(),
-            "USB устройства": Peripherals_Info.get_Unix_USB_Devices(),
-            "Данные о GPU": GPU_Info.get_Unix_CGPU_Info(),
-            "Данные о RAM памяти": Memory_Info.get_Common_Memory_Info(),
-            "Сетевые настройки": Network_Info.get_Common_Network_Info(),
-            "Приложения": Software_Info.get_xdg_open_applications(),
-            "Исполняемые файлы PATH": Software_Info.get_executables_in_PATH(),
-        }
+        report = {k: audit_dct[k] for k in keys}
         # Запись отчёта в JSON файл
         with open(json_file_path, 'w') as file:
             json.dump(report, file, ensure_ascii=False, indent=2)
